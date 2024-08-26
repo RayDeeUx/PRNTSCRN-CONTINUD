df7d007a69f16e3729bdab3efd45fec737eb22a5
ninXout <58670749+ninXout@users.noreply.github.com>
2024-08-26 13:38:11 -0400

    i forgot the parenthesis :(

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ void copy_screenshot(std::unique_ptr<uint8_t[]> data, const CCSize& size, bool c
 			}
 		} else {
 			CCImage* image = new CCImage();
-			std::string filepath = geode::Mod::get()->getConfigDir() / "test.png").string();
+			std::string filepath = (geode::Mod::get()->getConfigDir() / "test.png").string();
 			image->initWithImageData((void*)data, (int)size.width * (int)size.height * 4, CCImage::EImageFormat::kFmtPng, (int)size.width, (int)size.height, 8);
 			image->saveToFile(filepath.c_str(), true);
 		}
