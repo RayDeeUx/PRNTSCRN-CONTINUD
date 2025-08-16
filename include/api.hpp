4d0d106c3b500cde45d87ddad5de3ea817de6463
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-08-16 14:15:13 -0400

    i did a stupit

--- include/api.hpp
+++ include/api.hpp
@@ -43,17 +43,17 @@ namespace PRNTSCRN { // Pretty Rad (and) Nifty Tool (to) Screen Capture Right No
 
 	inline void screenshotNodeUsingTagFrom(CCNode* parent, int tag) {
 		if (!parent) return log::error("[PRNTSCRN API] unable to reference parent node");
-		if (tag < 0) return log::error("[PRNTSCRN API] unable to fetch child by tag, tag is less than 0");
+		if (tag < 0) return log::error("[PRNTSCRN API] unable to fetch child by tag, tag {} is less than 0", tag);
 		CCNode* node = parent->getChildByTag(tag);
 		if (!node) return log::error("[PRNTSCRN API] unable to reference node using screenshotNodeUsingTagFrom with tag {}", tag);
 		PRNTSCRN::screenshotNode(node);
 	}
 
 	template<class T>
-	void screenshotNodeByTypeFrom(CCNode* parent, T type, int index) {
+	void screenshotNodeByTypeFrom(CCNode* parent, int index) {
 		if (!parent) return log::error("[PRNTSCRN API] unable to reference parent node using screenshotNodeByTypeFrom");
-		CCNode* node = parent->getChildByType<type>(index);
-		if (!node) return log::error("[PRNTSCRN API] unable to reference node using screenshotNodeByTypeFrom");
+		CCNode* node = parent->getChildByType<T>(index);
+		if (!node) return log::error("[PRNTSCRN API] unable to reference node of type {} and index {} using screenshotNodeByTypeFrom", typeid(T).name(), index);
 		PRNTSCRN::screenshotNode(node);
 	}
 }
