19f8da50e98e92c3489915ce494297ce05c67e08
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-08-16 15:13:58 -0400

    API shuffling (added demagle and logging options)

--- include/api.hpp
+++ include/api.hpp
@@ -3,12 +3,6 @@
 using namespace geode::prelude;
 
 namespace PRNTSCRN { // Pretty Rad (and) Nifty Tool (to) Screen Capture Right Now
-	enum ReferenceType {
-		ByID = 0,
-		ByIDRecursive = 1,
-		ByQuerySelector = 2
-	};
-
     class ScreenshotEvent final : public Event {
 	    protected:
 	        CCNode* nodeToScreenshot {};
@@ -16,6 +10,33 @@ namespace PRNTSCRN { // Pretty Rad (and) Nifty Tool (to) Screen Capture Right No
 	        explicit ScreenshotEvent(CCNode* node) : nodeToScreenshot(node) {}
 	        [[nodiscard]] CCNode* getNode() const { return nodeToScreenshot; }
     };
+
+	enum ReferenceType {
+		ByID = 0,
+		ByIDRecursive = 1,
+		ByQuerySelector = 2
+	};
+
+	// THIS FUNCTION IS FOR LOGGING PURPOSES ONLY.
+	// YOU SHOULD NOT NEED THIS IN 99.9% OF CASES.
+	// THE DOUBLE UNDERSCORES SHLD ILLUSTRATE THAT
+	// code happily reused from geode-sdk/DevTools
+	inline std::string __demangle__(const char* typeName) {
+        #ifdef GEODE_IS_WINDOWS
+		return typeName + 6;
+        #else
+		std::string ret;
+
+		int status = 0;
+		auto demangle = abi::__cxa_demangle(typeName, 0, 0, &status);
+		if (status == 0) {
+			ret = demangle;
+		}
+		free(demangle);
+
+		return ret;
+        #endif
+	}
 
 	inline void screenshotNode(CCNode* node) {
 		if (!node) return log::error("[PRNTSCRN API] unable to reference node from screenshotNode");
@@ -53,7 +74,7 @@ namespace PRNTSCRN { // Pretty Rad (and) Nifty Tool (to) Screen Capture Right No
 	void screenshotNodeByTypeFrom(CCNode* parent, int index) {
 		if (!parent) return log::error("[PRNTSCRN API] unable to reference parent node using screenshotNodeByTypeFrom");
 		CCNode* node = parent->getChildByType<T>(index);
-		if (!node) return log::error("[PRNTSCRN API] unable to reference node of type {} and index {} using screenshotNodeByTypeFrom", typeid(T).name(), index);
+		if (!node) return log::error("[PRNTSCRN API] unable to reference node of type {} and index {} using screenshotNodeByTypeFrom", PRNTSCRN::__demangle__(typeid(T).name()), index);
 		PRNTSCRN::screenshotNode(node);
 	}
 }
