0340f5cd6d530fe041299721dc8923c43499ebd8
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-08-22 18:29:20 -0400

    logssssssssssssssssssssssssssssssssssssssssss

--- src/Screenshot.cpp
+++ src/Screenshot.cpp
@@ -45,22 +45,34 @@ void Screenshot::intoFile(const std::string& filename, bool isFromPRNTSCRNAndWan
 	}).detach();
 	*/
 	std::thread([=, this, data = std::move(m_data)]() {
+		log::info("GLubyte* newData = new GLubyte[m_width * m_width * 4];");
 		GLubyte* newData = new GLubyte[m_width * m_width * 4];
+		log::info("for (int i = 0; i < m_height; ++i){");
 		for (int i = 0; i < m_height; ++i){
+		log::info("memcpy(&newData[i * m_width * 4],");
 			memcpy(&newData[i * m_width * 4],
 					&data.get()[(m_height - i - 1) * m_width * 4],
 					m_width * 4);
 		}
+		log::info("CCImage image{};");
 		CCImage image{};
+		log::info("image.m_nBitsPerComponent = 8;");
 		image.m_nBitsPerComponent = 8;
+		log::info("image.m_nHeight = m_height;");
 		image.m_nHeight = m_height;
+		log::info("image.m_nWidth = m_width;");
 		image.m_nWidth = m_width;
+		log::info("image.m_bHasAlpha = true;");
 		image.m_bHasAlpha = true;
+		log::info("image.m_bPreMulti = false;");
 		image.m_bPreMulti = false;
+		log::info("image.m_pData = newData;");
 		image.m_pData = newData;
+		log::info("#ifdef GEODE_IS_WINDOWS");
 		#ifdef GEODE_IS_WINDOWS
 		image.saveToFile(filename.c_str(), true);
 		#elif defined(GEODE_IS_ANDROID)
+		log::info("auto result = imgp::encode::png((void*)(m_data.get()), m_width, m_height);");
 		auto result = imgp::encode::png((void*)(m_data.get()), m_width, m_height);
 		if (result.isOk()) geode::utils::file::writeBinary(filename, result.unwrap());
 		#endif
