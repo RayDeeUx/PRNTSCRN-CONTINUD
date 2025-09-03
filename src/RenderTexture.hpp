33c53e6e3f410058bace6ea00e601106aa8d6c12
Erymanthus | RayDeeUx <51521765+RayDeeUx@users.noreply.github.com>
2025-09-02 23:30:29 -0400

    @matcool

--- src/RenderTexture.hpp
+++ src/RenderTexture.hpp
@@ -1,3 +1,26 @@
+// MIT License
+//
+// Copyright (c) 2024 mat
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// MIT license comment block restored by raydeeux per mat's heavily implied request (https://discord.com/channels/911701438269386882/911702535373475870/1412638225637445683)
 #pragma once
 
 #include <Geode/Geode.hpp>
