2bed751648ec307046630e38bdbc8069ce2c6cdc
Erymanthus (RayDeeUx) <raydeeux@gmail.com>
2026-02-15 15:49:26 -0500

    "getsettingvalue<vector<keybind>>" "theres a toString methdo on it"

--- src/Manager.hpp
+++ src/Manager.hpp
@@ -1,13 +1,17 @@
 #pragma once
 
+/*
 #ifndef GEODE_IS_IOS
 #include <geode.custom-keybinds/include/Keybinds.hpp>
 #endif
+*/
 
 using namespace geode;
+/*
 #ifndef GEODE_IS_IOS
 using namespace keybinds;
 #endif
+*/
 
 class Manager {
 protected:
@@ -20,12 +24,13 @@ public:
 		return instance;
 	}
 	static std::string fetchKeybindsStrings() {
-		std::string bodyText = fmt::format("Screenshot the level: {}", getBindsStringFor("screenshot"_spr));
+		std::string bodyText = fmt::format("Screenshot the level: {}", getBindsStringFor("screenshot"));
 		bodyText += "\n";
-		bodyText += fmt::format("Screenshot the screen: {}", getBindsStringFor("plain-screenshot"_spr));
+		bodyText += fmt::format("Screenshot the screen: {}", getBindsStringFor("plain-screenshot"));
 		return bodyText;
 	}
 	static std::string getBindsStringFor(const std::string& key, bool hasColor = true) {
+		/*
 		#ifdef GEODE_IS_IOS
 		if (hasColor) return "<c_>N/A</c>";
 		return "N/A";
@@ -43,6 +48,17 @@ public:
 		if (hasColor) return "<c_>NONE SET</c>";
 		return "NONE SET";
 		#endif
+		*/
+		std::string toReturn;
+		for (auto bind : Mod::get()->getSettingValue<std::vector<Keybind>>(key)) {
+			if (hasColor) toReturn += bind.toString();
+			else toReturn += bind.toString();
+		}
+		if (toReturn.empty()) {
+			if (hasColor) return "<c_>N/A</c>";
+			else return "N/A";
+		}
+		return toReturn;
 	}
 	static std::string getColorlessBindsStringFor(const std::string& key) {
 		return Manager::getBindsStringFor(key, false);
